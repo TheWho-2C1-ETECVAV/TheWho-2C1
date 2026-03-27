b7ce2fe1913c68c3464582638daa6cf77f3b8fbe
TheWho <the.who.git@gmail.com>
2026-03-27 12:20:12 -0300

    adicionando lista 1

---
+++ Atividade Dupla/Thiago e Murilo Cremonesi/PHP_1.5/index.php
@@ -0,0 +1,39 @@
+<?php
+
+$n1 = $_GET["n1"];
+$n2 = $_GET["n2"];
+$n3 = $_GET["n3"];
+$n4 = $_GET["n4"];
+$n5 = $_GET["n5"];
+$n6 = $_GET["n6"];
+$n7 = $_GET["n7"];
+$n8 = $_GET["n8"];
+
+
+
+
+?>
+
+<!DOCTYPE html>
+
+<html lang="pt-br">
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <title>Resultado </title>
+
+</head>
+
+<body>
+
+<h1>Resultado</h1>
+
+<p>resultado: <?php echo $nfinal; ?></p>
+
+<a href="main.html">Voltar</a>
+
+</body>
+
+</html>
